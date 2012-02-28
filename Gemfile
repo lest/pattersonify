e0aaf8d0b18bcb0b226f3e45db46b5b62b3afb01
Sergey Nartimov <just.lest@gmail.com>
2012-02-28 20:58:28 +0300

    add blank index page

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'heroku'
 gem 'thin'
 gem 'sinatra'
+gem 'slim'
--- lib/pattersonify/app.rb
+++ lib/pattersonify/app.rb
@@ -1,7 +1,11 @@
+require 'bundler/setup'
 require 'sinatra/base'
+require 'slim'
 
 module Pattersonify
   class App < Sinatra::Base
-
+    get '/' do
+      slim :index
+    end
   end
 end
