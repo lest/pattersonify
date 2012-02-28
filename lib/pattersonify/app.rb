4aa47b09f7a2d15875ae3e384ce8f3055847424f
Sergey Nartimov <just.lest@gmail.com>
2012-02-28 23:06:13 +0300

    add dragonfly and example use

--- lib/pattersonify/app.rb
+++ lib/pattersonify/app.rb
@@ -1,10 +1,20 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'slim'
+require 'dragonfly'
 
 module Pattersonify
   class App < Sinatra::Base
     set :root, File.expand_path('../../..', __FILE__)
+
+    Dragonfly[:images].configure_with(:imagemagick) do |c|
+      c.url_format = '/media/:job'
+      c.allow_fetch_url = true
+      c.protect_from_dos_attacks = true
+      c.secret = settings.session_secret
+    end
+
+    use Dragonfly::Middleware, :images
 
     get '/' do
       slim :index
